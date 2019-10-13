1bc8b2728d4b53adf1627f36dfc4193321226c52
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-10-13 13:38:45 -0700

    removing bin and obj folders before migration

--- factioncli/processing/faction/database.py
+++ factioncli/processing/faction/database.py
@@ -6,20 +6,22 @@ from factioncli.processing.config import get_config
 from factioncli.processing.cli.printing import print_output, error_out
 from factioncli.processing.docker.container import get_container, execute_container_command
 
+
 def create_database_migration(name, container_name='faction_core_1'):
     print_output("Creating database migration..")
     core = get_container(container_name)
     name = name + "_" + secrets.token_hex(8)
-    result = execute_container_command(core, 'dotnet ef migrations add {0}'.format(name))
+    result = execute_container_command(core, 'rm -rf /app/obj && rm -rf /app/bin && dotnet ef migrations add {0}'.format(name))
     if result.exit_code != 0:
         error_out("Could create migration. Output from command: \n{0}".format(result.output))
     else:
         print_output("Database dropped.")
 
+
 def drop_database(container_name='faction_core_1'):
     print_output("Dropping database..")
     core = get_container(container_name)
-    result = execute_container_command(core, 'rm -rf /app/obj && rm -rf /app/bin && dotnet ef database drop --force')
+    result = execute_container_command(core, 'dotnet ef database drop --force')
     if result.exit_code != 0:
         error_out("Could not drop database. Output from command: \n{0}".format(result.output))
     else:
@@ -34,6 +36,7 @@ def update_database(container_name='faction_core_1'):
         error_out("Could not update database. Output from command: \n{0}".format(result.output))
     else:
         print_output("Database updated.")
+
 
 def remove_database_files():
     print_output("Removing database files..")
