34a94392a03e525b388ca12da7a8e474666fd38f
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-10-13 14:09:56 -0700

    moving artifact clean up to seperate function

--- factioncli/processing/faction/database.py
+++ factioncli/processing/faction/database.py
@@ -7,11 +7,28 @@ from factioncli.processing.cli.printing import print_output, error_out
 from factioncli.processing.docker.container import get_container, execute_container_command
 
 
+def _cleanup_build_artifacts(container_name='faction_core_1'):
+    print_output("Cleaning build artifacts from Core..")
+    core = get_container(container_name)
+    bin_result = execute_container_command(core, 'rm -rf /app/bin')
+    obj_result = execute_container_command(core, 'rm -rf /app/obj')
+    success = True
+    if bin_result.exit_code != 0:
+        error_out("Could not clean up build artifacts. Output from rm -rf /app/bin: \n{0}".format(bin_result.output))
+        success = False
+    if bin_result.exit_code != 0 or obj_result.exit_code != 0:
+        error_out("Could not clean up build artifacts. Output from rm -rf /app/obj: \n{0}".format(obj_result.output))
+        success = False
+    if success:
+        print_output("Migration created.")
+        
+
 def create_database_migration(name, container_name='faction_core_1'):
+    _cleanup_build_artifacts()
     print_output("Creating database migration..")
     core = get_container(container_name)
     name = name + "_" + secrets.token_hex(8)
-    result = execute_container_command(core, 'rm -rf /app/obj && rm -rf /app/bin && dotnet ef migrations add {0}'.format(name))
+    result = execute_container_command(core, 'dotnet ef migrations add {0}'.format(name))
     if result.exit_code != 0:
         error_out("Could create migration. Output from command: \n{0}".format(result.output))
     else:
